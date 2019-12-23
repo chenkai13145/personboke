b1fe529a90f841ee92eefb9ab7185d334e82e1cd
chenkai <1263455889@qq.com>
2019-12-23 11:11:34 +0800

    css

--- src/views/experience/component/info.vue
+++ src/views/experience/component/info.vue
@@ -2,6 +2,10 @@
     <div class="info">
         <h2 class="title">{{data.title}}</h2>
         <p class="desc">{{data.desc}}</p>
+        <div v-for="(item,index) in data.descobj.arr" :key="index">
+            <h4 v-if="item.futitle">{{index+1}}、{{item.futitle}}</h4>
+            <pre class="textpre" v-if="item.daima">{{item.daima}}</pre>
+        </div>
     </div>
 </template>
 <script>
@@ -24,6 +28,15 @@ export default {
 .desc{
     text-align: center;
     color: rgb(161, 160, 160);
+}
+.textpre{
+    background-color: #f5f5f5;
+    padding: 8PX;
+    color: #000;
+    font-size: 13PX;
+    font-weight: bold;
+    border: 2PX solid #ccc;
+    border-radius: 5PX;
 }
 }
 </style>
--- src/views/experience/index.vue
+++ src/views/experience/index.vue
@@ -2,110 +2,158 @@
   <div class="exprence">
     <van-steps direction="vertical" :active="-1">
       <van-step v-for="(item,index) in data" :key="index">
-          <span class="time">
-              <div class="art-icon"></div>
-              <div class="art-time">{{item.time}}</div>
-          </span>
-          <div class="content">
-                 <h2 @click="pathTo(item)">{{item.title}}</h2>
-                 <div class="art">
-                          <img class="art_img" :src="item.imgurl"/>
-                          <p>{{item.desc}}</p>
-                 </div>
+        <span class="time">
+          <div class="art-icon"></div>
+          <div class="art-time">{{item.time}}</div>
+        </span>
+        <div class="content">
+          <h2 @click="pathTo(item)">{{item.title}}</h2>
+          <div class="art">
+            <img class="art_img" :src="item.imgurl" />
+            <p>{{item.desc}}</p>
           </div>
+        </div>
       </van-step>
-     
     </van-steps>
   </div>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            data:[
+  data() {
+    return {
+      data: [
+        {
+          title: "javascript基础点",
+          time: "2018-9-10",
+          imgurl: require("../../../public/img/icons/apple-touch-icon-60x60.png"),
+          desc: "js现在常用的数据类型",
+          descobj:{
+              id: 1,
+              arr: [
                 {
-                    title:'javascript基础点',
-                    time:'2018-9-10',
-                    imgurl:require('../../../public/img/icons/apple-touch-icon-60x60.png'),
-                    desc:'js现在常用的数据类型'
+                  futitle: "时尚大事件",
+                  daima: `<pre>吉萨大历史
+                  Observer.prototype = {
+  on: function (type, handle) {
+    if (!this.listeners[type]) this.listeners[type] = [];
+    this.listeners[type].push(handle)
+  },
+  <h2 class="title">{{data.title}}</h2>
+        <p class="desc">{{data.desc}}</p>
+        <div v-for="(item,index) in data.descobj.arr" :key="index">
+            <h4 v-if="item.futitle">{{item.futitle}}</h4>
+            <pre class="textpre" v-if="item.daima">{{item.daima}}</pre>
+        </div>
+                                          sljdlsa
+                                             逻辑上sddddddddddddddd的浪费时间看
+                                      </pre>`
                 },
-                 {
-                    title:'javascript基础点',
-                    time:'2018-9-10',
-                    imgurl:require('../../../public/img/icons/apple-touch-icon-60x60.png'),
-                    desc:'js现在常用的数据类型'
+                {
+                  futitle: "时尚大事件",
+                  daima: `<pre>
+                                   吉萨大历史
+                                      sljdlsa
+                                             逻辑上的浪aaaaaaaaaaaaaa费时间看
+                            </pre>`
                 },
-                 {
-                    title:'javascript基础点',
-                    time:'2018-9-10',
-                    imgurl:require('../../../public/img/icons/apple-touch-icon-60x60.png'),
-                    desc:'js现在常用的数据类型'
+                {
+                  futitle: "时尚大事件",
+                  daima: `<pre>
+                                   吉萨大历史
+                                      sljdlsa
+                                             逻辑上ssssssssssssssss的浪费时间看
+                            </pre>`
                 },
-                 {
-                    title:'javascript基础点',
-                    time:'2018-9-10',
-                    imgurl:require('../../../public/img/icons/apple-touch-icon-60x60.png'),
-                    desc:'js现在常用的数据类型'
+                {
+                  futitle: "时尚大事件",
+                  daima: `<pre>
+                                   吉萨大历史
+                                      sljdsssssssssssssslsa
+                                             逻辑上的浪费kkkkkkkkkk时间看
+                            </pre>`
                 }
-            ]
+              ]
+            }
+        },
+        {
+          title: "javascript基础点",
+          time: "2018-9-10",
+          imgurl: require("../../../public/img/icons/apple-touch-icon-60x60.png"),
+          desc: "js现在常用的数据类型"
+        },
+        {
+          title: "javascript基础点",
+          time: "2018-9-10",
+          imgurl: require("../../../public/img/icons/apple-touch-icon-60x60.png"),
+          desc: "js现在常用的数据类型"
+        },
+        {
+          title: "javascript基础点",
+          time: "2018-9-10",
+          imgurl: require("../../../public/img/icons/apple-touch-icon-60x60.png"),
+          desc: "js现在常用的数据类型"
         }
-    },
-    methods:{
-        pathTo(data){
-            this.$router.push({path:'/info',query:{data:JSON.stringify(data)}})
-        }
+      ]
+    };
+  },
+  methods: {
+    pathTo(data) {
+      this.$router.push({
+        path: "/info",
+        query: { data: JSON.stringify(data) }
+      });
     }
+  }
 };
 </script>
 <style lang="scss" scoped>
-/deep/ .van-step__circle{
-    width: 20px;
-    height: 20px;
+/deep/ .van-step__circle {
+  width: 20px;
+  height: 20px;
 }
-/deep/ .van-steps{
-    background:rgba(0,0,0,0);
+/deep/ .van-steps {
+  background: rgba(0, 0, 0, 0);
 }
-.exprence{
-    .time{
-         display: flex;
-         
-        position: absolute;
-        top:-10px;
-        .art-icon{
-            border-right: 16px solid rgb(214, 216, 218);
-            border-bottom: 24px solid transparent;
-            border-top: 24px solid transparent;
-        }
-        .art-time{
-        width: 120px;
-        height: 50px;
-        line-height: 50px;
-        text-align: center;
-        background-color: rgb(214, 216, 218);
-        font-size: 16px;
-        }
-    }
-    .content{
-        padding: 30px;
-        h2:hover{
-         text-decoration: underline;
-         cursor: pointer;
-        }
-        .art{
-            .art_img{
-                max-width: 600PX;
-                max-height: 300PX;
-                display: block;
-            }
-        }
-    }
-    .van-step--vertical{
-        position: relative;
-        margin: 20px 0;
+.exprence {
+  .time {
+    display: flex;
 
+    position: absolute;
+    top: -10px;
+    .art-icon {
+      border-right: 16px solid rgb(214, 216, 218);
+      border-bottom: 24px solid transparent;
+      border-top: 24px solid transparent;
     }
-    .van-steps--vertical{
-        padding: 20px 40px;
+    .art-time {
+      width: 120px;
+      height: 50px;
+      line-height: 50px;
+      text-align: center;
+      background-color: rgb(214, 216, 218);
+      font-size: 16px;
     }
+  }
+  .content {
+    padding: 30px;
+    h2:hover {
+      text-decoration: underline;
+      cursor: pointer;
+    }
+    .art {
+      .art_img {
+        max-width: 600px;
+        max-height: 300px;
+        display: block;
+      }
+    }
+  }
+  .van-step--vertical {
+    position: relative;
+    margin: 20px 0;
+  }
+  .van-steps--vertical {
+    padding: 20px 40px;
+  }
 }
 </style>
